b613fa7c832dae23a17773362c9938e392503244
1337-hacks <elijahnucum.exe@gmail.com>
2021-08-14 16:51:37 +1200

    commit - bug fixes - site is working

--- Documents/Uni/Third Year/CISE/CISE_Repos/cise_ass1a_worksheet4/frontend/src/App.js
+++ Documents/Uni/Third Year/CISE/CISE_Repos/cise_ass1a_worksheet4/frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import {
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Redirect
 } from "react-router-dom";
 
 import Home from "./pages/home";
 import SEPractice from "./pages/SE-practice";
 import SubmitArticle from "./pages/submit-article"; 
-//import NotFoundPage from "./pages/404";
+import NotFoundPage from "./pages/404-page-error";
 
 const App = () =>  {
     return (
@@ -24,6 +25,8 @@ const App = () =>  {
             <Route exact path="/" component={Home}/>
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Redirect to="/404" />
           </div>
         </div>
         </Router>
--- Documents/Uni/Third Year/CISE/CISE_Repos/cise_ass1a_worksheet4/frontend/src/index.js
+++ Documents/Uni/Third Year/CISE/CISE_Repos/cise_ass1a_worksheet4/frontend/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import Main from "./App";
  
 ReactDOM.render(
-  //<App/>, 
+  <Main/>, 
   document.getElementById("root")
 );
